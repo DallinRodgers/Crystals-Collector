fab5089f6b0c8aabbb69f4d3142cd4e03ba3c9c3
DallinRodgers <rodgers.dallin@gmail.com>
2019-11-04 09:53:50 -0700

    Big Changes
    Removed Bootstrap 4,
    Removed original imgs,
    Added new jewel imgs,
    Created .flex and .center,
    Added background gradients,
    Set .container size,
    Set body min-height,
    Added jewel eventListeners,
    Created didYouWin function,
    Finished newGame function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,60 @@
-$(document).ready(function() {
-  var randomScore;
-  var playerScore;
-  var wins;
-  var loses;
-  var jewelsScores = [0, 0, 0, 0];
-  var gameIsPlaying;
+// $(document).ready(function() {
+var randomScore;
+var playerScore;
+var wins = 0;
+var loses = 0;
+var jewelsScores = [0, 0, 0, 0];
 
-  function newGame() {
-    //   Get new score to guess
-    randomScore = randomNumber(30);
-    // Get jewels scores
-    for (var i = 0; i < jewelsScores.length; i++) {
-      jewelsScores[i] = randomNumber(0);
-    }
-    // console.log(jewelsScores);
-    // Update UI
-    $("#numberToGuess").text(randomScore);
-    gameIsPlaying = true;
+function newGame() {
+  //   Get new score to guess
+  randomScore = randomNumber(30);
+  playerScore = 0;
+  // Get jewels scores
+  for (var i = 0; i < jewelsScores.length; i++) {
+    jewelsScores[i] = randomNumber(1);
   }
+  // Update UI
+  $("#yourScore").text(playerScore);
+  $("#numberToGuess").text(randomScore);
+}
 
-  function randomNumber(min) {
-    var randomNum = Math.floor(Math.random() * 20) + min;
-    return randomNum;
+function randomNumber(min) {
+  var randomNum = Math.floor(Math.random() * 20) + min;
+  return randomNum;
+}
+
+function didYouWin() {
+  if (playerScore === randomScore) {
+    wins += 1;
+    $("#wins").text(wins);
+    newGame();
+  } else if (playerScore > randomScore) {
+    loses += 1;
+    $("#loses").text(loses);
+    newGame();
   }
+}
 
-  $(".jewel-1").on("click", function() {
-    if (gameIsPlaying) {
-      console.log(jewelsScores[0]);
-    }
-  });
-  $(".jewel-2").on("click", function() {
-    console.log(jewelsScores[1]);
-  });
-  $(".jewel-3").on("click", function() {
-    console.log(jewelsScores[2]);
-  });
-  $(".jewel-4").on("click", function() {
-    console.log(jewelsScores[3]);
-  });
-
-  newGame();
+$(".jewel-1").on("click", function() {
+  playerScore += jewelsScores[0];
+  $("#yourScore").text(playerScore);
+  didYouWin();
 });
+$(".jewel-2").on("click", function() {
+  playerScore += jewelsScores[1];
+  $("#yourScore").text(playerScore);
+  didYouWin();
+});
+$(".jewel-3").on("click", function() {
+  playerScore += jewelsScores[2];
+  $("#yourScore").text(playerScore);
+  didYouWin();
+});
+$(".jewel-4").on("click", function() {
+  playerScore += jewelsScores[3];
+  $("#yourScore").text(playerScore);
+  didYouWin();
+});
+
+newGame();
+// });
